4f65176a6c6ab3a15fc9ae0afe41b5eab2a91d6f
nlist2 <noah2list@gmail.com>
2020-06-15 14:11:49 -0500

    main and readme changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PBR-Scrape
+    PBR-Scrape uses BeautifulSoup and Requests to scrape the baseball scouting platform, Prep Baseball Report
--- main_scrape.py
+++ main_scrape.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup as bs4
 import requests
+from login_function import pbr_login # this imports Atom's login function
 
