e5275d73f517c12d8b2acbdb1c379ca5d23711fc
nlist2 <noah2list@gmail.com>
2020-06-15 14:18:30 -0500

    main scrape test

--- main_scrape.py
+++ main_scrape.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup as bs4
+from bs4 import BeautifulSoup
 import requests
 from login_function import pbr_login # this imports Atom's login function
 
+# request test -> works for me...
+
+r = requests.get(
+    'https://en.wikipedia.org/wiki/Web_scraping')
+
+if r.status_code == 200:
+    soup = BeautifulSoup(r.content, "html.parser")
+    
+    for headline in soup.find_all("span", {"class": "mw-headline"}):
+        print(headline.text)
