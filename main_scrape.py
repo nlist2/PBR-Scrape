66d9e83b3e1d5e22dfe4180871db61a99f3dc24a
nlist2 <noah2list@gmail.com>
2020-06-16 12:20:40 -0500

    clean up

--- main_scrape.py
+++ main_scrape.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 # first build a functon that gets all of the data from a profile
 
 from bs4 import BeautifulSoup
@@ -9,10 +8,10 @@ import requests
 from login_function import pbr_login # this imports Atom's login function
 import csv
 
-# request test -> works for me...
-
-sess = pbr_login(requests.Session())
-
+try:
+    sess = pbr_login(requests.Session()) # tries to log in
+except:
+    print("Couldn't log in")
 
 def get_stats(url):
     ranking_list = []
@@ -42,7 +41,7 @@ def get_stats(url):
         # this chunk gets the reports
         try:
             for stat in soup.find_all("div", {"class": "comment"}):
-                    report_list.append(stat.text)
+                report_list.append(stat.text)
         except:    
             print("Report data not found.")
 
@@ -52,3 +51,8 @@ def get_stats(url):
         print("Status Code not 200")
 
 print(get_stats("https://www.prepbaseballreport.com/profiles/NY/Ian-Anderson-9408172536-8451697230#tab2"))
+
+"""
+for x in range(len(url_list)):
+    get_stats(url_list[x])
+"""
