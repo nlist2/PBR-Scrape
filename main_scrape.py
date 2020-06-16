73ff293eb2cced491ead096469c26dee1e5f2599
nlist2 <noah2list@gmail.com>
2020-06-16 11:11:16 -0500

    scrape push

--- main_scrape.py
+++ main_scrape.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+# first build a functon that gets all of the data from a profile
+
 from bs4 import BeautifulSoup
 import requests
 from login_function import pbr_login # this imports Atom's login function
+import csv
 
 # request test -> works for me...
 
 sess = pbr_login(requests.Session())
+stat_list = []
 
-r = requests.get(
-    'https://www.prepbaseballreport.com/profile-search-results')
+def get_stats(url)
+    r = requests.get(
+        url)
 
-if r.status_code == 200:
-    soup = BeautifulSoup(r.content, "html.parser")
-    
-    for headline in soup.find_all("tr", {"class": "alt"}):
-        print(headline.text)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "html.parser")
+        
+        for stat in soup.find_all("ul", {"class": "data-list"}):
+            for naked_stat in stat.find_all("strong"):
+                stat_list.append(naked_stat.text)
 
-    for player in soup.find_all("tr"):
-        print(player.text)
-
-    print(len(headline), len(player))
-else:
-    print("failed")
+    else:
+        print("Status Code not 200")
